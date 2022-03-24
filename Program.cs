bac048ba6d06aad28abd98bfe4b710fed5b41517
hristian-carabulea <hristian.carabulea@outlook.com>
2022-03-24 19:30:20 +0100

    Declare variables inline

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
 ﻿ class LargestOfThreeNumbers
     {
     /// <summary>
-    /// num1 = 60
-    /// num2 = 45
-    /// num3 = 123
-    /// Output: 123
+    /// find the largest number from the tree
+    /// num1 = 1
+    /// num2 = 3
+    /// num3 = 2
+    /// Output: 3
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args)
     {
-        int num1 = 0, num2 = 0, num3 = 0, tempNum = 0;
+        //int num1 = 0, num2 = 0, num3 = 0, tempNum = 0; // using instead inline declarations
 
         System.Console.WriteLine("Program will output the highest number from three values.");
 
         System.Console.Write("Enter first integer number: ");
         string num1String = System.Console.ReadLine();
         // display error message if input not a number
-        if (!System.Int32.TryParse(num1String, out num1))
+        if (!System.Int32.TryParse(num1String, out int num1))
         {
             System.Console.WriteLine("Wrong input");
         }
@@ -24,7 +25,7 @@
         {
             System.Console.Write("Enter second integer number: ");
             string num2String = System.Console.ReadLine();
-            if (!System.Int32.TryParse(num2String, out num2))
+            if (!System.Int32.TryParse(num2String, out int num2))
             {
                 System.Console.WriteLine("Wrong input");
             }
@@ -32,13 +33,13 @@
             {
                 System.Console.Write("Enter third integer number: ");
                 string num3String = System.Console.ReadLine();
-                if (!System.Int32.TryParse(num3String, out num3))
+                if (!System.Int32.TryParse(num3String, out int num3))
                 {
                     System.Console.WriteLine("Wrong input");
                 }
                 else
                 {
-                    tempNum = num1;
+                    int tempNum = num1;
 
                     if (num1 < num2) { 
                         tempNum = num2;
